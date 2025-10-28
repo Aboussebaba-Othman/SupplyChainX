a509a9ab76ed1671a2cb145e992eb6622835b92b
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2025-10-28 15:09:35 +0100

    first commit

---
+++ src/main/java/com/supplychainx/supplychainx/SupplyChainXApplication.java
@@ -0,0 +1,13 @@
+package com.supplychainx.supplychainx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyChainXApplication.class, args);
+    }
+
+}
