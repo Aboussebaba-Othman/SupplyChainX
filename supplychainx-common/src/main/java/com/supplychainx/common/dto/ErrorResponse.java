0dda7688197c7d09604fc10cf06821d7ddf16ddf
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2025-10-29 22:23:05 +0100

    add les class dto

---
+++ supplychainx-common/src/main/java/com/supplychainx/common/dto/ErrorResponse.java
@@ -0,0 +1,36 @@
+package com.othman.exemple.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<ErrorDetail> details;
+    
+    public static ErrorResponse of(int status, String error, String message, String path) {
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status)
+                .error(error)
+                .message(message)
+                .path(path)
+                .build();
+    }
+}
